8d848135c09e21e8fc530f1f830d039001e73595
dstejas19 <tejas@live.in>
2020-11-12 23:54:38 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,11 +1,10 @@
 let config = {};
 
-config.username = 'dstejas19'; // GitHub username
-config.password = 'tejchinran2012'; // GitHub password
-config.privateKey = '86dcf1ad86fe6065c5c272959ae1e29bb0e22062'; // GitHub privatekey
-config.repoName = 'LeetCode-Solutions-test'; // GitHub repo name, it can be an existing one. If it does not exist, a new one in this name will be created
-config.repoDesc = 'Solutions for Leetcode problems in C++'; // Description for repo in case a new one is created
-config.dir =
-  '/media/dstejas19/f6e268cf-f181-438b-a727-fb517362c1c2/Leetcode_Solutions'; // Directory of local repo. It may or may not exist, if it doesn't a new one will be created
+config.username = ''; // GitHub username
+config.password = ''; // GitHub password
+config.privateKey = ''; // GitHub http access key
+config.repoName = ''; // GitHub repo name, it can be an existing one. If it does not exist, a new one in this name will be created
+config.repoDesc = ''; // Description for repo in case a new one is created
+config.dir = ''; // Directory of local repo. It may or may not exist, if it doesn't a new one will be created
 
 module.exports = config;
